9534195619feaaa912e3398f3e9ff645d22f7f36
iktalk <iktalk.94@gmail.com>
2022-05-15 13:09:48 -0400

    create basic search engine

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React from "react";
 import "./App.css";
+import Dictionary from "./Dictionary";
 
 export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>Hello</p>
-      </header>
+    <div className="container">
+      <div className="App">
+        <header className="App-header">
+          <h1>Dictionary App</h1>
+          <Dictionary />
+        </header>
+      </div>
     </div>
   );
 }
