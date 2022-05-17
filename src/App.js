84771e988522da5e5527dbee1476095a25860339
iktalk <iktalk.94@gmail.com>
2022-05-16 21:01:05 -0400

    Add footer and basic styling

--- src/App.js
+++ src/App.js
@@ -7,9 +7,15 @@ export default function App() {
     <div className="container">
       <div className="App">
         <header className="App-header">
-          <h1>Dictionary App</h1>
+          <h1 className="mt-3 mb-3">Dictionary App</h1>
           <Dictionary />
         </header>
+        <footer className="mt-5">
+          This application was coded by Ishmeet Kaur and is Open-Source on{" "}
+          <a href="https://github.com/iktalk/react-dictionary-app.git">
+            Github
+          </a>
+        </footer>
       </div>
     </div>
   );
