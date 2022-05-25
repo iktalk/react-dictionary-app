dfb93d4b6e2bdb2da932873898bc306a11e739b5
iktalk <iktalk.94@gmail.com>
2022-05-24 23:29:20 -0400

    render photos with search

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,13 +1,20 @@
 import React, { useState } from "react";
 import axios from "axios";
 import Results from "./Results";
+import Photos from "./Photos";
 
 export default function Dictionary() {
   let [keyword, setKeyword] = useState("");
   let [results, setResults] = useState(null);
+  let [photos, setPhotos] = useState(null);
 
   function handleResponse(response) {
     setResults(response.data[0]);
+  }
+
+  function handlePexelResponse(response) {
+    console.log(response.data);
+    setPhotos(response.data.photos);
   }
 
   function search(event) {
@@ -15,6 +22,16 @@ export default function Dictionary() {
 
     let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
     axios.get(apiUrl).then(handleResponse);
+
+    let pexelsApiKey =
+      "563492ad6f917000010000015a7568321ee545839a17283b2e00c767";
+    let pexelsApiUrl = `https://api.pexels.com/v1/search?query=${keyword}&per_page=6`;
+
+    axios
+      .get(pexelsApiUrl, {
+        headers: { Authorization: `Bearer ${pexelsApiKey}` },
+      })
+      .then(handlePexelResponse);
   }
 
   function handleKeywordChange(event) {
@@ -28,6 +45,7 @@ export default function Dictionary() {
         <input type="submit" value="Search" />
       </form>
       <Results results={results} />
+      <Photos photos={photos} />
     </div>
   );
 }
